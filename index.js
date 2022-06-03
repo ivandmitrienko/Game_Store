f96dcef4a95fb7e9ad2c1c8543fce925a721ec5c
ivandmitrienko <oneblackcrystal@gmail.com>
2022-06-03 23:53:40 +0300

    i

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// import { render } from 'react-dom'
+window.React = React
+
 import App from './components/App';
 import { BrowserRouter } from "react-router-dom";
 
