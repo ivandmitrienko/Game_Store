01e1e3cfed39815fe2a427eabcc1cb186c9871b6
ivandmitrienko <oneblackcrystal@gmail.com>
2022-06-03 21:34:29 +0300

    i

--- components/service.js
+++ components/service.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function service() {
     return (
-        <>
+        <div>
             <h2>This is a service page</h2>
-        </>
+        </div>
     )
 }
