0d72bd7ffd24c3c1cd2efd1a7f59f8ee66489058
ivandmitrienko <oneblackcrystal@gmail.com>
2022-06-03 21:34:35 +0300

    i

--- components/footer.js
+++ components/footer.js
@@ -4,8 +4,8 @@ import React from 'react'
 
 export default function footer() {
     return (
-        <>
+        <div>
             <h6>This is a footer component</h6>
-        </>
+        </div>
     )
 }
--- components/header.js
+++ components/header.js
@@ -3,12 +3,12 @@ import {Link} from 'react-router-dom'
 
 export default function header() {
     return (
-        <>
+        <div>
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/service">Service</Link></li>
             </ul>
-        </>
+        </div>
     )
 }
