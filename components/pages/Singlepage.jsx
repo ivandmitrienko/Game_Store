ba417f58ea47a34d25aec804b3dde5e0747f6a7d
ivandmitrienko <oneblackcrystal@gmail.com>
2022-06-04 22:11:28 +0300

    initial commit

--- components/pages/Singlepage.jsx
+++ components/pages/Singlepage.jsx
@@ -1,5 +1,5 @@
 import { useParams } from 'react-router-dom';
-// import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 // const Singlepage = () => {
 //     const {id} = useParams();
@@ -34,10 +34,22 @@ import { useParams } from 'react-router-dom';
 const Singlepage = () => {
 
     const {id} = useParams();
+    const [post, setPost] = useState(null);
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+            .then(res => res.json())
+            .then(data => setPost(data))
+    }, [id]);
 
     return (
         <div>
-            {id}
+           {post && (
+                <div>
+                    <h1>{post.title}</h1>
+                    <p>{post.body}</p>
+                </div>)
+            }   
         </div>
     )
 }
