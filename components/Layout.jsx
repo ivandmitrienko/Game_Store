960b77622aed9568945f7ade83e87c1b253c6410
ivandmitrienko <oneblackcrystal@gmail.com>
2022-06-04 16:58:23 +0300

    initial commit

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,15 +1,17 @@
 import { NavLink, Outlet } from "react-router-dom";
 
+const setActive = ({isActive}) => ({color:isActive ? 'var(--color-active)': 'white'});
+
 const Layout = () => {
     
     return (
         <div>
             <header>
                 <span>Game Store</span>
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/action">Action</NavLink>
-                <NavLink to="/strategy">Strategy</NavLink>
-                <NavLink to="/fantasy">Fantasy</NavLink>
+                <NavLink to="/" style={setActive}>Home</NavLink>
+                <NavLink to="/action" style={setActive}>Action</NavLink>
+                <NavLink to="/strategy" style={setActive}>Strategy</NavLink>
+                <NavLink to="/fantasy" style={setActive}>Fantasy</NavLink>
             </header>
             <main className="container">
               <Outlet />
