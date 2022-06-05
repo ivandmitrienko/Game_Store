e78edc989bfb398e9358b2c07a7bba1ad3671dc2
ivandmitrienko <oneblackcrystal@gmail.com>
2022-06-06 01:00:51 +0300

    initial commit

--- components/pages/Gamelist.jsx
+++ components/pages/Gamelist.jsx
@@ -1,17 +1,23 @@
 import React from "react";
+import "./Gamelist.css"
 
-const Gamelist = ({games,title}) => {
+const Gamelist = ({games}) => {
 
   
-    return (
-              <div>
-               {games.map((e) => (
-               <div key={e.id}>
-                       <h2>{e.title}</h2>
-                       <h3>{e.price}</h3>
-                </div>
-               ))}     
-              </div>
+    return (      
+            <div className="items">
+                {games.map((e) => (
+                    <div className="game-item" key={e.id}>
+                        <div className="game-item__details">
+                            <span className="game-item__title">{e.title}</span>
+                            <div className="game-item__genre">
+                                {e.genres}
+                            </div>
+                            <div className="game-item__buy">Buy</div>
+                        </div>
+                    </div>
+                ))}
+            </div>        
           )
   
     
