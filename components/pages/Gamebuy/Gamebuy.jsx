58bbf5c941b84bdbdef331f71406231525f1e428
ivandmitrienko <oneblackcrystal@gmail.com>
2022-06-07 00:49:25 +0300

    initial commit

--- components/pages/Gamebuy/Gamebuy.jsx
+++ components/pages/Gamebuy/Gamebuy.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { setItemInBasket } from '../../Redux/Basket/reducer'
 import './Gamebuy.css';
 import { Button } from '../../Button/Button.jsx';
 
 const Gamebuy = ({game}) => {
 
-  
+    const dispatch = useDispatch();
+
+    const handleClicke = (EO)=>{
+        EO.stopPropagation();
+        dispatch(setItemInBasket(game));
+
+    }; 
+
+
     return (      
             <div className="game-buy">
                 <span className="game-buy__price">{game.price} руб.</span>
-                    <Button onClick={()=>null} type="primary">
+                    <Button onClick={handleClicke} type="primary">
                      В Корзину
                     </Button>   
             </div>        
