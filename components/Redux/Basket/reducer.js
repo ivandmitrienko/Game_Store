69f29d44ef9abfb0c5a0a5af5ce413cde1bcbf2d
ivandmitrienko <oneblackcrystal@gmail.com>
2022-06-07 00:12:45 +0300

    initial commit

---
+++ components/Redux/Basket/reducer.js
@@ -0,0 +1,21 @@
+import { greatSlice } from "@reduxjs/toolkit";
+
+const basketSlice = greatSlice({
+    name: 'Basket',
+    initialState:{
+        itemsInBasket: []
+    },
+    reducers:{
+        setItemInBasket: (state, action)=>{
+            state.itemsInBasket.push(action.payload)
+        },
+        deleteItemFromBasket:(state, action)=>{
+            state.itemsInBasket = state.itemsInBasket.filter(game => game.id !== action.payload)
+        }
+    }
+
+    
+});
+
+export const { setItemInBasket, deleteItemFromBasket } = basketSlice.actions;
+
