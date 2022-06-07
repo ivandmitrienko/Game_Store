2231c6901542d1ae298ec20c4bfea3ed7d4e14b2
ivandmitrienko <oneblackcrystal@gmail.com>
2022-06-07 17:00:27 +0300

    штшешфд сщььше

--- components/pages/Gamebuy/Gamebuy.jsx
+++ components/pages/Gamebuy/Gamebuy.jsx
@@ -1,25 +1,30 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { setItemInBasket } from '../../Redux/Basket/reducer'
+import { useDispatch, useSelector } from "react-redux";
+import { setItemInBasket, deleteItemFromBasket } from '../../Redux/Basket/reducer'
 import './Gamebuy.css';
 import { Button } from '../../Button/Button.jsx';
 
 const Gamebuy = ({game}) => {
 
     const dispatch = useDispatch();
+    const items = useSelector(state => state.basket.itemsInBasket);
+    const isItemInBasket = items.some(items=>items.id===game.id); //находится ли элемент в корзине
 
-    const handleClicke = (EO)=>{
+    const handleClicke = (EO)=>{// кладем элемент или убираем из корзины
         EO.stopPropagation();
-        dispatch(setItemInBasket(game));
-
+        if(isItemInBasket){
+            dispatch(deleteItemFromBasket(game.id))
+        } else {
+            dispatch(setItemInBasket(game));
+        }
     }; 
 
 
     return (      
             <div className="game-buy">
                 <span className="game-buy__price">{game.price} руб.</span>
-                    <Button onClick={handleClicke} type="primary">
-                     В Корзину
+                    <Button onClick={handleClicke}  type={isItemInBasket ? 'secondary':'primary'}>
+                    {isItemInBasket ? 'Убрать из корзины': "В корзину"}
                     </Button>   
             </div>        
           )
