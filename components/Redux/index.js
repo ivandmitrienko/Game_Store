3b431eac370e67361a350e0317970f92e0bd9a9b
ivandmitrienko <oneblackcrystal@gmail.com>
2022-06-08 00:11:54 +0300

    initial commit

--- components/Redux/index.js
+++ components/Redux/index.js
@@ -2,8 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import basketReducer from "./Basket/reducer";
 
+import gamesReducer from "./Games/reducer";
+
 export const store = configureStore({
     reducer: {
         basket: basketReducer,
+        game: gamesReducer,
     },
 });
--- components/pages/Gamelist/Gamelist.jsx
+++ components/pages/Gamelist/Gamelist.jsx
@@ -1,16 +1,23 @@
 import React from "react";
-import "./Gamelist.css";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import { Gamecover } from '../Gamecover.jsx';
 import { Gamebuy } from '../Gamebuy/Gamebuy.jsx';
 import { Gamegenre } from '../../Genre/Genre.jsx';
+import { setCurrentGame } from '../../Redux/Games/reducer';
+import "./Gamelist.css";
 
 const Gamelist = ({games}) => {
 
-  
+    const navigate = useNavigate();
+    const handleClick = () => {dispatch(setCurrentGame(games));
+    navigate(`/${games.title}`);}// игра сразу же окажется в redux и переместится на другую страницу 
+    const dispatch = useDispatch();
+
     return (      
             <div className="items">
                 {games.map((game) => (
-                    <div className="game-item" key={game.id}>
+                    <div className="game-item" key={game.id} onClick={handleClick}>
                         <Gamecover image={game.image} />
                         <div className="game-item__details">
                             <span className="game-item__title">{game.title}</span>
