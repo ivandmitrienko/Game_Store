b4ab9cf1f24eaacaf768cd834bbaa3f0f63a08ca
ivandmitrienko <oneblackcrystal@gmail.com>
2022-06-10 18:06:38 +0300

    initial commit

--- components/pages/Category.jsx
+++ components/pages/Category.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Gamelist } from './Gamelist/Gamelist.jsx';
+import { Button } from '../Button/Button.jsx';
 
 export const Category = () => {
 
@@ -9,14 +10,36 @@ export const Category = () => {
     const [games, setGames] = useState(null);
 
     useEffect(() => {
-        fetch(`http://localhost:3000/games?genres=${genres}`)
-        .then(res => res.json())
-        .then(data => setGames(data))
+        getGames(6);
     }, [genres]);
+
+    const getGames =(quantity)=>{
+        fetch(`http://localhost:3000/games?genres=${genres}&_limit=${quantity}`)
+        .then(res => res.json())
+        .then(data => setGames(data));
+
+    }
+
+    // useEffect(() => {
+    //     fetch(`http://localhost:3000/games?genres=${genres}&_limit=6`)
+    //     .then(res => res.json())
+    //     .then(data => setGames(data))
+    // }, [genres]);
     
     return (
           <div className='Games'>
             {games && <Gamelist games={games} key={games.id}/>}
+            <div className='allButtons'>
+                <Button type = {"third"} onClick={()=>getGames(54)}>
+                        Показать все
+                </Button>
+                <Button type = {"third"} onClick={()=>getGames(54)}>
+                        Вперед
+                </Button>
+                <Button type = {"third"} onClick={()=>getGames(54)}>
+                        Назад
+                </Button>
+            </div>
         </div>    
     )
 }
