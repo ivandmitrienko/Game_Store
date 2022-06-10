70a356314c5715cb2e6dc8c6ec7942d10035b7ba
ivandmitrienko <oneblackcrystal@gmail.com>
2022-06-10 16:48:10 +0300

    initial commit

--- components/pages/Homepage.jsx
+++ components/pages/Homepage.jsx
@@ -1,23 +1,36 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-// import { ButtonsPaginate } from '../ButtonsPaginate/ButtonsPaginate.jsx';
+import { Button } from '../Button/Button.jsx';
 import { Gamelist } from './Gamelist/Gamelist.jsx';
+
 const Homepage = () => {
 
-    let [games, setLimitGames] = useState(null);
+    const [games, setLimitGames] = useState(null);
 
     useEffect(() => {
-        fetch(`http://localhost:3000/games`)
-        .then(res => res.json())
-        .then(data => setLimitGames(data))
+        getGames(6);
     }, []);
 
+   
+
+    const getGames =(quantity)=>{
+        fetch(`http://localhost:3000/games?_limit=${quantity}`)
+        .then(res => res.json())
+        .then(data => setLimitGames(data))
+
+    }
+
+    //TODO удалить кнопку
+
     // ?&_limit=18 
-    
+
     return (
         <div className='Games'>  
+            <h1>Избранное</h1>
             {games && <Gamelist games={games} key={games.id}/>}
+            <Button type = {"third"}>Показать все</Button> 
         </div>
+        
        
             
     )
